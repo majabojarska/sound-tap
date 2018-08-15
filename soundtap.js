08c554f52656526e1c9ac65b8f2d22f218d6ffeb
majabojarska <majabojarska98@gmail.com>
2018-08-15 12:59:09 +0200

    reduce max hue change speed

--- soundtap.js
+++ soundtap.js
@@ -4,157 +4,131 @@ var soundMap = {
   //////////////////////////////////////////////////////////////////
   // Small-case letters
   q: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/chord1.mp3"]
     })
   },
   w: {
-    // color: 'red',
     sound: new Howl({
       src: ["audio/Guitar/chord3.mp3"]
     })
   },
   e: {
-    // color: 'cyan',
     sound: new Howl({
       src: ["audio/Guitar/chord4.mp3"]
     })
   },
   r: {
-    // color: 'blue',
     sound: new Howl({
       src: ["audio/Guitar/chord5.mp3"]
     })
   },
   t: {
-    // color: 'limegreen',
     sound: new Howl({
       src: ["audio/Guitar/chord6.mp3"]
     })
   },
   y: {
-    // color: 'steelblue',
     sound: new Howl({
       src: ["audio/Leszek/fenk.mp3"]
     })
   },
   u: {
-    // color: 'yellow',
     sound: new Howl({
       src: ["audio/Leszek/lup1.mp3"]
     })
   },
   i: {
-    // color: 'pink',
     sound: new Howl({
       src: ["audio/Leszek/picobello.mp3"]
     })
   },
   o: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Leszek/FesteB.mp3"]
     })
   },
   p: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Leszek/pk1.mp3"]
     })
   },
   a: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/penta1.mp3"]
     })
   },
   s: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/penta2.mp3"]
     })
   },
   d: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/penta3.mp3"]
     })
   },
   f: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/penta4.mp3"]
     })
   },
   g: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/penta5.mp3"]
     })
   },
   h: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/penta6.mp3"]
     })
   },
   j: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/penta7.mp3"]
     })
   },
   k: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/penta8.mp3"]
     })
   },
   l: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/penta9.mp3"]
     })
   },
   z: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/perc1.mp3"]
     })
   },
   x: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/perc3.mp3"]
     })
   },
   c: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/perc2.mp3"]
     })
   },
   v: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/mute1.mp3"]
     })
   },
   b: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/bend1.mp3"]
     })
   },
   n: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/chord2.mp3"]
     })
   },
   m: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/misc3.mp3"]
     })
@@ -163,157 +137,131 @@ var soundMap = {
   //////////////////////////////////////////////////////////////////
   // Capital letters
   Q: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   W: {
-    // color: 'red',
     sound: new Howl({
       src: [""]
     })
   },
   E: {
-    // color: 'cyan',
     sound: new Howl({
       src: [""]
     })
   },
   R: {
-    // color: 'blue',
     sound: new Howl({
       src: [""]
     })
   },
   T: {
-    // color: 'limegreen',
     sound: new Howl({
       src: [""]
     })
   },
   Y: {
-    // color: 'steelblue',
     sound: new Howl({
       src: [""]
     })
   },
   U: {
-    // color: 'yellow',
     sound: new Howl({
       src: [""]
     })
   },
   I: {
-    // color: 'pink',
     sound: new Howl({
       src: [""]
     })
   },
   O: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   P: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Leszek/pk1_echo.mp3"]
     })
   },
   A: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   S: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   D: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   F: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   G: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   H: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   J: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   K: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   L: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   Z: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   X: {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/perc4.mp3"]
     })
   },
   C: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   V: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   B: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   N: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   M: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
@@ -322,61 +270,51 @@ var soundMap = {
   //////////////////////////////////////////////////////////////////
   // Numbers
   "1": {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/bass2.mp3"]
     })
   },
   "2": {
-    // color: 'red',
     sound: new Howl({
       src: ["audio/Guitar/bass3.mp3"]
     })
   },
   "3": {
-    // color: 'cyan',
     sound: new Howl({
       src: ["audio/Guitar/bass4.mp3"]
     })
   },
   "4": {
-    // color: 'blue',
     sound: new Howl({
       src: ["audio/Guitar/bass5.mp3"]
     })
   },
   "5": {
-    // color: 'limegreen',
     sound: new Howl({
       src: ["audio/Guitar/bass6.mp3"]
     })
   },
   "6": {
-    // color: 'limegreen',
     sound: new Howl({
       src: ["audio/Guitar/bass7.mp3"]
     })
   },
   "7": {
-    // color: 'limegreen',
     sound: new Howl({
       src: ["audio/Guitar/bass8.mp3"]
     })
   },
   "8": {
-    // color: 'limegreen',
     sound: new Howl({
       src: ["audio/Guitar/bass9.mp3"]
     })
   },
   "9": {
-    // color: 'limegreen',
     sound: new Howl({
       src: ["audio/Guitar/bass10.mp3"]
     })
   },
   "0": {
-    // color: 'limegreen',
     sound: new Howl({
       src: ["audio/Guitar/bass11.mp3"]
     })
@@ -385,55 +323,46 @@ var soundMap = {
   //////////////////////////////////////////////////////////////////
   // Special letters
   ą: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   ę: {
-    // color: 'red',
     sound: new Howl({
       src: [""]
     })
   },
   ó: {
-    // color: 'cyan',
     sound: new Howl({
       src: [""]
     })
   },
   ś: {
-    // color: 'blue',
     sound: new Howl({
       src: [""]
     })
   },
   ł: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   ż: {
-    // color: 'red',
     sound: new Howl({
       src: [""]
     })
   },
   ź: {
-    // color: 'cyan',
     sound: new Howl({
       src: [""]
     })
   },
   ć: {
-    // color: 'blue',
     sound: new Howl({
       src: [""]
     })
   },
   ń: {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
@@ -442,193 +371,161 @@ var soundMap = {
   //////////////////////////////////////////////////////////////////
   // Symbols
   "!": {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   "@": {
-    // color: 'red',
     sound: new Howl({
       src: [""]
     })
   },
   "#": {
-    // color: 'cyan',
     sound: new Howl({
       src: [""]
     })
   },
   $: {
-    // color: 'blue',
     sound: new Howl({
       src: [""]
     })
   },
   "%": {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   "^": {
-    // color: 'red',
     sound: new Howl({
       src: [""]
     })
   },
   "&": {
-    // color: 'cyan',
     sound: new Howl({
       src: [""]
     })
   },
   "*": {
-    // color: 'blue',
     sound: new Howl({
       src: [""]
     })
   },
   "(": {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   ")": {
-    // color: 'red',
     sound: new Howl({
       src: [""]
     })
   },
   "-": {
-    // color: 'cyan',
     sound: new Howl({
       src: ["audio/Guitar/bass12.mp3"]
     })
   },
   _: {
-    // color: 'blue',
     sound: new Howl({
       src: [""]
     })
   },
   "+": {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   "=": {
-    // color: 'red',
     sound: new Howl({
       src: ["audio/Guitar/bass13.mp3"]
     })
   },
   "[": {
-    // color: 'cyan',
     sound: new Howl({
       src: ["audio/Guitar/disson1.mp3"]
     })
   },
   "]": {
-    // color: 'blue',
     sound: new Howl({
       src: ["audio/Guitar/disson2.mp3"]
     })
   },
   "{": {
-    // color: 'white',
     sound: new Howl({
       src: [""]
     })
   },
   "}": {
-    // color: 'red',
     sound: new Howl({
       src: [""]
     })
   },
   ":": {
-    // color: 'cyan',
     sound: new Howl({
       src: [""]
     })
   },
   ";": {
-    // color: 'blue',
     sound: new Howl({
       src: ["audio/Guitar/penta10.mp3"]
     })
   },
   "'": {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/penta11.mp3"]
     })
   },
   '"': {
-    // color: 'red',
     sound: new Howl({
       src: [""]
     })
   },
   "\\": {
-    // color: 'cyan',
     sound: new Howl({
       src: ["audio/Guitar/penta12.mp3"]
     })
   },
   "|": {
-    // color: 'blue',
     sound: new Howl({
       src: [""]
     })
   },
   ",": {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/misc6.mp3"]
     })
   },
   "<": {
-    // color: 'red',
     sound: new Howl({
       src: [""]
     })
   },
   ".": {
-    // color: 'cyan',
     sound: new Howl({
       src: ["audio/Guitar/misc11.mp3"]
     })
   },
   ">": {
-    // color: 'blue',
     sound: new Howl({
       src: [""]
     })
   },
   "/": {
-    // color: 'white',
     sound: new Howl({
       src: ["audio/Guitar/harmonics1.mp3"]
     })
   },
   "?": {
-    // color: 'red',
     sound: new Howl({
       src: [""]
     })
   },
   "#": {
-    // color: 'cyan',
     sound: new Howl({
       src: [""]
     })
   },
   "`": {
-    // color: 'blue',
     sound: new Howl({
       src: ["audio/Guitar/bass1.mp3"]
     })
@@ -637,7 +534,6 @@ var soundMap = {
   //////////////////////////////////////////////////////////////////
   // Whitespace
   " ": {
-    // color: 'blue',
     sound: new Howl({
       src: ["audio/Guitar/thump1.mp3"]
     })
@@ -678,7 +574,7 @@ function onKeyDown(event) {
     newCircle.endPoint = endPoint;
     var randomMoveSpeed = Math.random() * 5;
     newCircle.moveSpeed = randomMoveSpeed;
-    var randomHueChangeSpeed = Math.random() * 3;
+    var randomHueChangeSpeed = Math.random() * 2;
     newCircle.hueChangeSpeed = randomHueChangeSpeed;
     // <!-- give newCircle a predefined color -->
     // newCircle.fillColor = soundMap[event.key].color;
