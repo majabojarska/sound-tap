1476cbff5d7796579e783862f823a8f1c6d557a6
majabojarska <majabojarska98@gmail.com>
2018-08-16 12:13:13 +0200

    js libs and img loading

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,67 +1,82 @@
-const path = require("path")
+const path = require("path");
 
 module.exports = {
-    entry: {
-        main: "./src/main.js"
-    },
-    mode: "development",
-    output: {
-        filename: "[name]-bundle.js",
-        path: path.resolve(__dirname, "../dist"),
-        publicPath: "/"
-    },
-    devServer: {
-        contentBase: "dist",
-        overlay: true,
-        stats: {
-            colors: true
-        },
-    },
-    module: {
-        rules: [{
-                test: /\.css$/,
-                use: [{
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    }
-                ]
-            },
-            {
-                test: /\.html$/,
-                use: [{
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].html"
-                        }
-                    },
-                    {
-                        loader: "extract-loader"
-                    },
-                    {
-                        loader: "html-loader",
-                    }
-                ]
-            }, {
-                test: /\.(png|jpe?g|svg)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: 'img/[name].[ext]',
-                    }
-                }]
-            },
-            // {
-            //     test: /\.(png|jpe?g|svg)$/,
-            //     use: [{
-            //         loader: 'url-loader',
-            //         options: {
-            //             limit: 8000, // Convert images < 8kb to base64 strings
-            //             name: 'img/[name].[ext]',
-            //         }
-            //     }]
-            // }
-        ]
+  entry: {
+    main: "./src/main.js"
+  },
+  mode: "development",
+  output: {
+    filename: "assets/js/[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "dist",
+    overlay: true,
+    stats: {
+      colors: true
     }
-}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].html"
+            }
+          },
+          {
+            loader: "extract-loader"
+          },
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "assets/img/[name].[ext]"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: "file-loader",
+        include: /assets/,
+        options: {
+          name: "assets/js/[name].[ext]"
+        }
+      }
+      // url-loader
+      // {
+      //     test: /\.(png|jpe?g|svg)$/,
+      //     use: [{
+      //         loader: 'url-loader',
+      //         options: {
+      //             limit: 8000, // Convert images < 8kb to base64 strings
+      //             name: 'img/[name].[ext]',
+      //         }
+      //     }]
+      // }
+    ]
+  }
+};
