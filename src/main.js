fac686223138ed50a6a3516ca16efd63153a3613
majabojarska <majabojarska98@gmail.com>
2018-08-16 12:14:00 +0200

    require js libraries and scripts not meant for bundling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
-require("./main.css");
-require("./img/black-linen.png");
+require("./assets/js/about.js");
+require("./assets/js/howler.js");
+require("./assets/js/paper.js");
+require("./assets/js/soundtap.js");
+require("./assets/css/main.css");
+require("./assets/img/black-linen.png");
 require("./index.html");
